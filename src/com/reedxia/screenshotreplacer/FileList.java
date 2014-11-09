09544fde0f884477930de87851d80e7a30c76731
Reed Xia <huaqin2005@gmail.com>
2014-11-09 11:47:58 +0800

    Refine FileList and Replacer class, add class Main as entry.

--- src/com/reedxia/screenshotreplacer/FileList.java
+++ src/com/reedxia/screenshotreplacer/FileList.java
@@ -5,37 +5,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileList {
+    
+    // FileList类将帮忙寻找指定路径下所有的PNG文件，并可生成列表 － List<File>
 
     public static void main(String[] args) {
         FileList a = new FileList(new File("/Users/reed/Desktop"));
         for (File x : a.getPNGList()) {
             System.out.println(x.getAbsolutePath());
+            System.out.println(x.getParent());
+            System.out.println(x.getName());
+            System.out.println("----------------------");
+
         }
     }
 
     FileList(File path) {
-
+        // 构造方法，参数是一个类型为File的参数，必须是一个有效的的目录的绝对路径，否则将抛出异常IllegalArgumentException
         if (path.isDirectory() && path.isAbsolute()) {
             this.entryPath = path;
         } else {
             throw new IllegalArgumentException("The given path is not absolute directory!!");
         }
         
-        pngFileList = new ArrayList<File>();
+        pngFileList = new ArrayList<File>(); // 初始化png文件列表
     }
 
-    private File entryPath;
-    private List<File> pngFileList;
+    private File entryPath; // 寻找后缀名是.png文件的入口路径，也即实例化FileList类所传入构造方法的path参数
+    private List<File> pngFileList; // 对象类型为File的列表，即将把找到的所有png文件添加到该列表，该列表的初始化在构造方法中完成
 
     public List<File> getPNGList() {
-
+        // 寻找PNG文件，填充PNG文件列表 － pngFileList，并返回该文件列表
         this.fillPNGList(entryPath);
         return pngFileList;
     }
 
     private void fillPNGList(File absolutePath) {
-
-        File[] fileListInDir = absolutePath.listFiles();
+        //寻找指定路径下的所有png文件，并添加到pngFileList文件列表，
+        //对该路径下的子目录继续执行本方法
+        File[] fileListInDir = absolutePath.listFiles(); //，用以保存指定路径下所有文件&目录列表，注意目录也是File类型
 
         for (int i = 0; i < fileListInDir.length; i++) {
 
@@ -43,7 +50,7 @@ public class FileList {
 
                 if (fileListInDir[i].getAbsolutePath().endsWith(".png")) {
                     
-                    System.out.println(fileListInDir[i].getAbsolutePath());
+//                    System.out.println(fileListInDir[i].getAbsolutePath());
                     this.pngFileList.add(fileListInDir[i]);
                 }
             } else {
@@ -51,5 +58,7 @@ public class FileList {
                 this.fillPNGList(fileListInDir[i]);
             }
         }
+        fileListInDir = null; // 此时该临时文件列表已完成使用
+        
     }
 }
--- src/com/reedxia/screenshotreplacer/Replacer.java
+++ src/com/reedxia/screenshotreplacer/Replacer.java
@@ -7,11 +7,23 @@ import javax.imageio.ImageIO;
 
 public class Replacer {
 
-    Replacer(BufferedImage statusBarImage, BufferedImage screenshotImage) {
-
-        // 构造函数，需要两个image
-        this.setStatusBarImage(statusBarImage);
-        this.setScreenshotImage(screenshotImage);
+    Replacer(File statusBarImageFile, File screenshotImageFile) {
+        
+        System.out.println("## Replacer is working on - " + screenshotImageFile.getAbsolutePath());
+        
+        // 如果参数的文件名不是.png文件结尾，将抛出参数错误异常
+        if (!statusBarImageFile.getAbsolutePath().endsWith(".png") || !screenshotImageFile.getAbsolutePath().endsWith(".png")) {
+            throw new IllegalArgumentException("The given files are not .png file!");
+        }
+        
+        // 构造方法，需要两个image
+        try {
+            this.setStatusBarImage(ImageIO.read(statusBarImageFile));
+            this.setScreenshotImage(ImageIO.read(screenshotImageFile));
+        } catch (IOException e) {
+            System.out.println(e.getLocalizedMessage());
+            e.printStackTrace();
+        }
 
         wOfNewScreen = screenshot.getWidth(); // 合并后新图的宽，也即待修改图片screenshot的宽
         hOfStatusBar = statusBarImage.getHeight(); // 状态栏图的高，合并后的图的上半部分
@@ -19,8 +31,12 @@ public class Replacer {
 
         if (wOfNewScreen != statusBarImage.getWidth() || hOfStatusBar >= hOfScreenshot) {
             // 状态栏图和待修改图screenshot的宽必须相同，同时状态栏图的高不应大于screenshot
-            throw new IllegalArgumentException("The two images don't have same width or status bar image has longer height!!");
+            throw new IllegalArgumentException("The two images don't have same width or status bar image has longer height!");
         }
+        
+        // 构造合并后的新图的绝对路径，同时给文件名加上前缀 "new_"
+        abosolutePathOfNewScreen = screenshotImageFile.getParent() + "/" + "new_" + screenshotImageFile.getName();
+
     }
 
     private int wOfNewScreen; // 合并后新图的宽
@@ -30,6 +46,8 @@ public class Replacer {
     // 初始化Replacer类的实例所必需的两个参数，分别为一个小图（状态栏图）和一个大图
     private BufferedImage statusBarImage;
     private BufferedImage screenshot;
+    
+    private String abosolutePathOfNewScreen;
 
     private void setStatusBarImage(BufferedImage newStatusBarImage) {
         this.statusBarImage = newStatusBarImage;
@@ -64,19 +82,18 @@ public class Replacer {
         return statusBarImage.getRGB(0, 0, wOfNewScreen, hOfStatusBar, statusBarImageArray, 0, wOfNewScreen);
     }
 
-    public void outputNewScreen(File newScreenshotFile) {
+    public void outputNewScreen() {
 
         try {
             // 初始化合并后的新图
             BufferedImage newScreenshot = new BufferedImage(wOfNewScreen, hOfScreenshot, screenshot.getType());
             // 设置新图的上半部分的RGB数据
             newScreenshot.setRGB(0, 0, wOfNewScreen, hOfStatusBar, this.getStatusBarRGBArray(), 0, wOfNewScreen);
-            // 测试数据
-            System.out.println(hOfScreenshot);
-            System.out.println(hOfStatusBar);
+
             // 设置新图的下半部分的RGB数据
             newScreenshot.setRGB(0, hOfStatusBar, wOfNewScreen, hOfScreenshot - hOfStatusBar, this.getBottomPartRGBArray(), 0, wOfNewScreen);
-            ImageIO.write(newScreenshot, "png", newScreenshotFile);
+            
+            ImageIO.write(newScreenshot, "png", new File(abosolutePathOfNewScreen));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -84,9 +101,9 @@ public class Replacer {
 
     public static void main(String[] args) throws IOException {
 
-        File statusBarImage = new File("/Users/reed/Desktop/small.png"); // 已经修改好的状态栏图片
-        File screenshot = new File("/Users/reed/Desktop/big.png"); // 待修改的完整图片
-        Replacer replacer = new Replacer(ImageIO.read(statusBarImage), ImageIO.read(screenshot));
-        replacer.outputNewScreen(new File("/Users/reed/Desktop/out.png"));
+        File statusBarImageFile = new File("/Users/reed/Desktop/small.png"); // 已经修改好的状态栏图片
+        File screenshotFile = new File("/Users/reed/Desktop/big.png"); // 待修改的完整图片
+        Replacer replacer = new Replacer(statusBarImageFile, screenshotFile);
+        replacer.outputNewScreen();
     }
 }
